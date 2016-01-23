60057cad64201920a59cd7f74f840b9a87c1fd72
lesspion <coquelet.c@gmail.com>
2016-01-23 11:40:08 +0100

    initialize skeleton express

---
+++ onepoint_adress_book_back/routes/geoloc.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    
+});
+
+router.post('/', function (req, res) {
+    
+});
+
+router.put('/', function (req, res) {
+    
+});
+
+router.delete('/:id', function (req, res) {
+    
+});
+
+module.exports = router;
