849b0b4810343cc5d3d8c0cd1363a43464677747
lesspion <coquelet.c@gmail.com>
2016-01-23 12:41:50 +0100

    create function to update the json file

--- onepoint_adress_book_back/my_modules/putAddressJSON.js
+++ onepoint_adress_book_back/my_modules/putAddressJSON.js
@@ -1,5 +1,43 @@
-var putAdressJSON = function () {
+var fs = require('fs');
+var _ = require('lodash');
+var jsonfile = require('jsonfile');
+
+var containsObject = function (object, json) {
+   for (var i = 0; i < json.length; i++) {
+        if (_.isEqual(object, json[i])) {
+            return true;
+        }
+    }
+
+    return false; 
+};
+
+var mergeObject = function (obj1, obj2) {
+    for (var i in obj2) {
+        obj1[i] = obj2[i];
+    }
+    return obj1;
+}
+
+var putAdressJSON = function (address, objectLatLng) {
+    var adress = address || null;
+    var jsonAdresses = JSON.parse(fs.readFileSync(__dirname + '/../public/json/addressBook.json', 'utf8'));
+    var result = containsObject(adress, jsonAdresses);
+    if (result) {
+        for (var i = 0; i < jsonAdresses.length; i++) {
+            if (_.isEqual(adress, jsonAdresses[i])) {
+                jsonAdresses[i] = mergeObject(jsonAdresses[i], objectLatLng);
+            }
+        }
+    } else {
+        jsonAdresses.push(address);
+    }
     
+    jsonfile.writeFile(__dirname + '/../public/json/addressBook.json', jsonAdresses, function (err) {
+        console.error(err);
+    });
+    
+    return true;
 };
 
 module.exports = putAdressJSON;
